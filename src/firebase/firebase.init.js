27566dca024a17fb501161280d86c58e67f85991
Meherin-Islam <meherinislam59@gmail.com>
2024-12-22 14:41:35 +0600

    Firebase SetUp Done

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlsag9xno7wWbW66CjXsgh1czW9drzHiY",
+  authDomain: "fashion-fusion-b2c9f.firebaseapp.com",
+  projectId: "fashion-fusion-b2c9f",
+  storageBucket: "fashion-fusion-b2c9f.firebasestorage.app",
+  messagingSenderId: "498524220549",
+  appId: "1:498524220549:web:a2a38e21292cca966c0dec"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/shared/NavBar.jsx
+++ src/shared/NavBar.jsx
@@ -13,7 +13,7 @@ const links = <>
 
 
     return (
-        <div className="navbar bg-base-100">
+        <div className="navbar bg-base-100 py-5">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
